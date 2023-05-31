409e18a9f06f633a6064a9593b4f5253c8d65e29
iamtanmay <iamtanmay@gmail.com>
2023-05-31 12:50:39 +0200

    Update localstack_init.sh

--- localstack_init.sh
+++ localstack_init.sh
@@ -3,14 +3,14 @@ docker rm /localstack_main
 export DEBUG=0
 export LOCALSTACK_VOLUME_DIR='/home/iamtanmay/localstack'
 export LOCAL_STACK_API_KEY=
+export AWS_ACCESS_KEY_ID="test"
+export AWS_SECRET_ACCESS_KEY="test"
+export AWS_DEFAULT_REGION="eu-west-1"
 
 #Add alias
 if ! grep -q "awslocal" ~/.bashrc ; then
   echo $'alias awslocal=\'aws --endpoint-url http://localhost:4566\'' >> ~/.bashrc
 fi
-export AWS_ACCESS_KEY_ID="test"
-export AWS_SECRET_ACCESS_KEY="test"
-export AWS_DEFAULT_REGION="eu-west-1"
 
 cd $LOCALSTACK_VOLUME_DIR
 docker compose up
